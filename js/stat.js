184ab367a58a79aee45c8a7b5af1bea9e17d5566
Andrey Bulaev <bulayka89@gmail.com>
2018-09-02 22:21:27 +0300

    Исправлена ошибка неверной отрисовки столбцов

--- js/stat.js
+++ js/stat.js
@@ -9,7 +9,7 @@ var COLUMN_GAP = 50;
 var FONT_GAP = 20;
 var BAR_WIDTH = 40;
 var GIST_HEIGHT = 150;
-var barHeight = GIST_HEIGHT - 2 * FONT_GAP;
+var barHeight = GIST_HEIGHT - FONT_GAP;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -35,13 +35,13 @@ window.renderStatistics = function (ctx, players, times) {
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
-  ctx.fillText('Ура вы победили!', CLOUD_X + 2 * GAP, CLOUD_Y + 2 * GAP);
-  ctx.fillText('Список результатов:', CLOUD_X + 2 * GAP, CLOUD_Y + 2 * GAP + FONT_GAP);
+  ctx.fillText('Ура вы победили!', CLOUD_X + 2 * GAP, CLOUD_Y + 3 * GAP);
+  ctx.fillText('Список результатов:', CLOUD_X + 2 * GAP, CLOUD_Y + 3 * GAP + FONT_GAP);
 
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < players.length; i++) {
-    ctx.fillText(players[i], CLOUD_X + GAP + i * (COLUMN_GAP + BAR_WIDTH), CLOUD_Y + CLOUD_HEIGHT - GAP - FONT_GAP);
+    ctx.fillText(players[i], CLOUD_X + 2 * GAP + i * (COLUMN_GAP + BAR_WIDTH), CLOUD_Y + CLOUD_HEIGHT - GAP - FONT_GAP);
 
     if (players[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
@@ -49,15 +49,9 @@ window.renderStatistics = function (ctx, players, times) {
       ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random() + ')';
     }
 
-    ctx.beginPath();
-    ctx.moveTo(CLOUD_X + GAP + i * (COLUMN_GAP + BAR_WIDTH), CLOUD_HEIGHT - GAP - FONT_GAP);
-    ctx.lineTo(CLOUD_X + GAP + i * (COLUMN_GAP + BAR_WIDTH), (barHeight * times[i]) / maxTime);
-    ctx.lineTo(CLOUD_X + GAP + i * (COLUMN_GAP + BAR_WIDTH) + BAR_WIDTH, (barHeight * times[i]) / maxTime);
-    ctx.lineTo(CLOUD_X + GAP + i * (COLUMN_GAP + BAR_WIDTH) + BAR_WIDTH, CLOUD_HEIGHT - GAP - FONT_GAP);
-    ctx.closePath();
-    ctx.fill();
+    ctx.fillRect(CLOUD_X + 2 * GAP + i * (COLUMN_GAP + BAR_WIDTH), CLOUD_HEIGHT - GAP - FONT_GAP - (barHeight * times[i]) / maxTime, BAR_WIDTH, (barHeight * times[i]) / maxTime);
 
     ctx.fillStyle = '#000';
-    ctx.fillText(Math.round(times[i]), CLOUD_X + GAP + i * (COLUMN_GAP + BAR_WIDTH), (barHeight * times[i]) / maxTime - FONT_GAP);
+    ctx.fillText(Math.round(times[i]), CLOUD_X + 2 * GAP + i * (COLUMN_GAP + BAR_WIDTH), CLOUD_HEIGHT - GAP - FONT_GAP - (barHeight * times[i]) / maxTime - FONT_GAP);
   }
 };
